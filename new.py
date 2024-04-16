38a7c374b5a80884a0cd9c0fa918caa92b9a1446
Ali Lamsalek <ALI.LAMSALEK.04@edu.uiz.ac.ma>
2024-04-16 11:22:50 +0100

    using string function in python

--- new.py
+++ new.py
@@ -1,1 +1,9 @@
 print("I am ali")
+print("hello world")
+a = "ALI LAMSALEK"
+print(a.title())
+print(a.capitalize())
+b, c, d = "10", "2025", "18"
+print(b.zfill(4))
+print(c.zfill(4))
+print(d.zfill(4))
